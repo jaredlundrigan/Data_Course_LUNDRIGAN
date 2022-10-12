24a5d5034d9134bd240a639546bd1ebf61add03b
Jared Lundrigan <jaredlundrigan@Jareds-MacBook-Air.local>
2022-10-12 17:20:22 -0600

    This is my final Assignment 7 answers.

--- Assignments/Assignment_7/7_answers_10.10.22.R
+++ Assignments/Assignment_7/7_answers_10.10.22.R
@@ -4,85 +4,122 @@ files <- list.files(pattern=".csv", full.names = TRUE)
 files
 readLines(files[5], n=1)
 df <- read_csv(files[5])
-view(df)
-df
+
 #Import the Assignment_7/Utah_Religions_by_County.csv data set
 
 #Clean it up into “tidy” shape
 
-#Explore the cleaned data set with a series of figures (I want to see you exploring the data set)
-
-#Address the questions:
-  
-#“Does population of a county correlate with the proportion of any specific religious group in that county?”
-#“Does proportion of any specific religion in a given county correlate with the proportion of non-religious people?”
-#Just stick to figures and maybe correlation indices…no need for statistical tests yet
-
 #Add comment lines that show your thought processes _____________
-
-
-
 
 # clean the data using janitor package
 
 library(janitor)
 library(tidyverse)
-names(df)  
-
-# 3 NEW LINE
-
-
 
 df <- df %>% janitor::clean_names()
 view(df)
-# do gitpull first to be able to push it to GitHub
+
 
 df <- df %>% 
-  pivot_longer(-c("county","pop_2010","religious"),
+  pivot_longer(cols = -c("county","pop_2010","religious"),
                names_to = "denomination",
                values_to = "frequency")
 
-df
+
+#Explore the cleaned data set with a series of figures (I want to see you exploring the data set)
+
 
 df %>% 
-  plot(x=df$County,y=df$Religious)
-
-plot1 <- df %>% 
-  ggplot(aes(x=County,
-             y=Religious)) +
-  geom_col()
-
-
-plot2 <- df %>% 
-  ggplot(aes(x=County,
-             y=Pop_2010)) +
-  geom_col()
+  ggplot(aes(x=pop_2010,
+             y=frequency,
+             color = denomination)) +
+  geom_point() +
+  geom_smooth(method="lm")
 
 df %>% 
-  group_by(Pop_2010) %>% 
+  group_by(pop_2010) %>% 
   ungroup() %>% 
-  arrange(desc(Pop_2010)) %>% 
-  summarize(County = County,
-            Pop_2010 = Pop_2010,
-            Religious = Religious,
-            LDS = LDS)
+  arrange(desc(pop_2010)) %>% 
+  summarize(county = county,
+            pop_2010 = pop_2010,
+            religious = religious,
+            denomination=denomination)
 
-df %>% cor(df$Pop_2010,df$Religious)
+plot(df)
+
+#Address the questions:
+
+#“Does population of a county correlate with the proportion of any specific religious group 
+# in that county?”
+
+# Yes, the proportion of muslims in a county correlates with the population. 
+# the correlation quotient is 76%. As the population of a county increases, 
+# so does the proportion of muslims (76% correlation)
+
+cor(df$pop_2010,df$frequency, use = "complete.obs")
+# no correlation
 
 df %>% 
-  ggplot(aes(x=LDS,
-             y=Pop_2010,
-             color = County)) +
-  geom_point()+
-  geom_smooth(method="lm") 
+  ggplot(aes(x=pop_2010,
+             y=frequency,
+             color = denomination)) +
+  geom_point() +
+  facet_wrap(~county)
+
+df1 <- subset(df, denomination== "lds")
+df2 <- subset(df, denomination== "non_religious")
+df3 <- subset(df, denomination== "catholic")
+df4 <- subset(df, denomination== "evangelical")
+df5 <- subset(df, denomination== "southern_baptist_convention")
+df6 <- subset(df, denomination== "assemblies_of_god")
+df7 <- subset(df, denomination== "episcopal_church")
+df8 <- subset(df, denomination== "pentecostal_church_of_god")
+df9 <- subset(df, denomination== "greek_orthodox")
+df10 <- subset(df, denomination== "united_methodist_church")
+df11 <- subset(df, denomination== "buddhism_mahayana")
+df12 <- subset(df, denomination== "muslim")
+df13 <- subset(df, denomination== "non_denominational")
+df14 <- subset(df, denomination== "orthodox")
+
+cor(df12$pop_2010,df12$frequency)
+# this shows a correlation in the muslim denomination
+
+df12 %>% 
+  ggplot(aes(x=pop_2010,
+             y=frequency,
+            color=county)) +
+  geom_point() +
+  geom_smooth(method="lm")
 
 
-df %>% 
-  pivot_longer(cols = df$`Assemblies of God`:df$Orthodox,
-               names_to = Denomination,
-               values_to = Proportion)
+#“Does proportion of any specific religion in a given county correlate with the 
+# proportion of non-religious people?”
 
-df
-?pivot_longer
+# df2 is non-religious
+df2 %>% 
+  ggplot(aes(x=pop_2010,
+             y=frequency,
+             color=county)) +
+  geom_point()
+
+cor(df$denomination == "orthodox", df$denomination == "non_religious")
+cor(df$denomination == "lds", df$denomination == "non_religious")
+cor(df$denomination == "catholic", df$denomination == "non_religious")
+cor(df$denomination == "evangelical", df$denomination == "non_religious")
+cor(df$denomination == "southern_baptist_convention", df$denomination == "non_religious")
+cor(df$denomination == "assemblies_of_god", df$denomination == "non_religious")
+cor(df$denomination == "episcopal_church", df$denomination == "non_religious")
+cor(df$denomination == "pentecostal_church_of_god", df$denomination == "non_religious")
+cor(df$denomination == "greek_orthodox", df$denomination == "non_religious")
+cor(df$denomination == "united_methodist_church", df$denomination == "non_religious")
+cor(df$denomination == "buddhism_mahayana", df$denomination == "non_religious")
+cor(df$denomination == "muslim", df$denomination == "non_religious")
+cor(df$denomination == "orthodox", df$denomination == "non_religious")
+cor(df$denomination == "non_denominational", df$denomination == "non_religious")
+
+
+# No, no specific religion in a given county correlates with non-religious people.
+
+#Just stick to figures and maybe correlation indices…no need for statistical tests yet
 
 
